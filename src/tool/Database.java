eabc71d1df4948b51021ed9b6e7e869725d4471c
suxiaocheng <suxiaocheng2010@gmail.com>
2019-03-16 15:40:35 +0800

    Create the project

---
+++ src/tool/Database.java
@@ -0,0 +1,185 @@
+package tool;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.tukaani.xz.LZMA2Options;
+import org.tukaani.xz.XZOutputStream;
+
+import app.GetHouseBuildingDetail;
+import config.AppConfig;
+import debug.Log;
+
+public class Database {
+	private static String databaseName = "hz_house";
+	protected static Connection conn = null;
+
+	private static final String strDBName = "hz_house.db";
+	private static final String strDBXZName = strDBName + ".xz";
+
+	static {
+		/* Check for dir */
+		File fDir = new File(AppConfig.WORKING_DIR);
+		if (fDir.exists() == true) {
+			if (fDir.isDirectory() == false) {
+				fDir.delete();
+			}
+		} else {
+			fDir.mkdirs();
+		}
+		
+		databaseName = "jdbc:sqlite:" + AppConfig.WORKING_DIR + databaseName + ".db";
+		try {
+			Class.forName("org.sqlite.JDBC");
+			conn = (Connection) DriverManager.getConnection(databaseName);
+		} catch (SQLException e) {
+			Log.e("Connection error!");
+			e.printStackTrace();
+			System.exit(-1);
+		} catch (ClassNotFoundException e) {
+			Log.e("Class no found error!");
+			e.printStackTrace();
+			System.exit(-2);
+		}
+		Log.d("Database: " + databaseName + " load sucessfully!");
+	}
+
+	public static boolean execSqlTable(String statement) {
+		boolean status = true;
+
+		try {
+			Statement stmt = conn.createStatement();
+			stmt.executeUpdate(statement);
+			stmt.close();
+		} catch (SQLException e) {
+			Log.e("Execute statement: " + statement + " error!");
+			e.printStackTrace();
+		}
+
+		return status;
+	}
+
+	public static String queryTable(String statement, String item) {
+		StringBuffer sb = new StringBuffer();
+
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(statement);
+			while (rs.next()) {
+				if (sb.length() > 0) {
+					sb.append(' ');
+				}
+				sb.append(rs.getString(item));
+			}
+			stmt.close();
+		} catch (SQLException e) {
+			Log.e("Execute statement: " + statement + " error!");
+			e.printStackTrace();
+			return null;
+		}
+
+		return sb.toString();
+	}
+
+	public static void closeDatabase() {
+		if (conn != null) {
+			try {
+				conn.close();
+				conn = null;
+				Log.d("Close database sucessfully!");
+			} catch (SQLException e) {
+				Log.e("Close database fail!");
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static String createTableNameStr(String name) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < name.length(); i++) {
+			Byte b[] = new Byte[2];
+			b[0] = (byte) ((name.charAt(i) & 0xf) + '0');
+			b[1] = (byte) (((name.charAt(i) >> 4) & 0xf) + '0');
+			sb.append(b[0] + b[1]);
+		}
+
+		return sb.toString();
+	}
+
+	public static String qureyForTable(String name) {
+		String sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='" + name + "';";
+		StringBuffer sb = new StringBuffer();
+
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				if (sb.length() > 0) {
+					sb.append(' ');
+				}
+				sb.append(rs.getString("name"));
+			}
+			stmt.close();
+		} catch (SQLException e) {
+			Log.e("Execute statement: " + sql + " error!");
+			e.printStackTrace();
+			return null;
+		}
+
+		return sb.toString();
+	}
+
+	public static void dropTable(String name) {
+		String sql = "DROP TABLE IF EXISTS " + name + " ;";
+		execSqlTable(sql);
+	}
+
+	public static String compressDB() {
+		LZMA2Options options = new LZMA2Options();
+		FileOutputStream outfile;
+		Log.d("Encoder memory usage: " + options.getEncoderMemoryUsage() + " KiB");
+		Log.d("Decoder memory usage: " + options.getDecoderMemoryUsage() + " KiB");
+		try {
+			File fDBOut = new File(AppConfig.WORKING_DIR + strDBXZName);
+			File fDBIn = new File(AppConfig.WORKING_DIR + strDBName);
+			if (fDBOut.exists() == true) {
+				fDBOut.delete();
+			}
+			outfile = new FileOutputStream(AppConfig.WORKING_DIR + strDBXZName);
+			XZOutputStream out = new XZOutputStream(outfile, options);
+			InputStream in = new FileInputStream(fDBIn);
+
+			byte[] buf = new byte[8192];
+			int size;
+			while ((size = in.read(buf, 0, buf.length)) != -1)
+				out.write(buf, 0, size);
+			out.finish();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			outfile = null;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			outfile = null;
+		}
+
+		if (outfile == null) {
+			return AppConfig.WORKING_DIR + strDBName;
+		}
+		return AppConfig.WORKING_DIR + strDBXZName;
+	}
+
+	public static void main(String[] args) {
+		compressDB();
+	}
+}
---
+++ src/tool/SendEmail.java
@@ -0,0 +1,111 @@
+package tool;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import debug.Log;
+
+public class SendEmail implements Runnable {
+	String text;
+	String filename;
+	String title;
+
+	public SendEmail(String _title, String _text, String _filename) {
+		super();
+		title = _title;
+		text = _text;
+		filename = _filename;
+	}
+
+	public void run() {
+		File attactment = null;
+		Properties props = new Properties();
+		// 开启debug调试
+		props.setProperty("mail.debug", "false");
+		// 发送服务器需要身份验证
+		props.setProperty("mail.smtp.auth", "true");
+		// 设置邮件服务器主机名
+		props.setProperty("mail.host", "smtp.163.com");
+		// 发送邮件协议名称
+		props.setProperty("mail.transport.protocol", "smtp");
+
+		// 设置环境信息
+		Session session = Session.getInstance(props);
+		session.setDebug(false);
+
+		// 创建邮件对象
+		Message msg = new MimeMessage(session);
+		try {
+			msg.setFrom(new InternetAddress("simulator_test@163.com"));
+
+			msg.setSubject(title);
+
+			// create and fill the first message part
+			MimeBodyPart mbp1 = new MimeBodyPart();
+			mbp1.setText(text);
+
+			MimeBodyPart mbp2 = null;
+			if (filename != null) {
+				attactment = new File(filename);
+				if(attactment.exists() == true){
+					// create the second message part
+					mbp2 = new MimeBodyPart();
+
+					// attach the file to the message
+					try {
+						mbp2.attachFile(attactment);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						Log.e("File attatch excepiton is happened");
+						filename = null;
+						e.printStackTrace();
+					}
+				}
+			}
+
+			// create the multi part and add its parts to it
+			Multipart mp = new MimeMultipart();
+			mp.addBodyPart(mbp1);
+			if (mbp2 != null) {
+				mp.addBodyPart(mbp2);
+			}
+
+			// add the multi part to the message
+			msg.setContent(mp);
+
+			// set the Date: header
+			msg.setSentDate(new Date());
+
+			Transport transport = session.getTransport();
+			// 连接邮件服务器
+			transport.connect("simulator_test", "simulator123");
+			// 发送邮件
+			transport.sendMessage(msg, new Address[] {
+					new InternetAddress("suxiaocheng2010@hotmail.com"),
+					new InternetAddress("simulator_test@163.com") });
+			// 关闭连接
+			transport.close();
+
+			Log.d("SendMail " + text + " successfully");
+
+		} catch (MessagingException e1) {
+			// TODO Auto-generated catch block
+			Log.e("Exception is happened: " + e1.getMessage());
+			e1.printStackTrace();
+		}
+		Log.d("SendMail thread is quit");
+	}
+}
